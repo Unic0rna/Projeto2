b6cfa3d2d54f8214d27f318c4c654a13185d67a6
Debora <ds336756@gmail.com>
2025-03-27 15:22:54 -0300

    Adicionando os códigos das controllers

---
+++ Projeto2/Controllers/ProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto2.Models;
+using Projeto2.Repositorio;
+
+namespace Projeto2.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public readonly ProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _produtoRepositorio.AdicionarProduto(produto);
+                return RedirectToAction("Login");
+            }
+
+            return View(produto);
+        }
+    }
+}
---
+++ Projeto2/Controllers/UsuarioController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto2.Models;
+using Projeto2.Repositorio;
+
+namespace Projeto2.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController (UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+
+            if (usuario != null &&usuario.Senha == senha)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Email ou senha inválidos.");
+
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");
+            }
+
+            return View(usuario);
+        }
+    }
+}
